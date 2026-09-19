6e4ae3deec6382a353c84581823e1cf937165a07
mahmoodmisix00-cell <mahmoodmisix00@gmail.com>
2026-09-19 12:25:36 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const STATIC_CACHE = 'beltfind-static-v8';
-const CDN_CACHE = 'beltfind-cdn-v8';
+const STATIC_CACHE = 'beltfind-static-v9';
+const CDN_CACHE = 'beltfind-cdn-v9';
 
 const APP_FILES = [
   './',
